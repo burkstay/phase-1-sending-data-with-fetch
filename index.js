c9c2d442104c7f9a6292e0a5eef4bc0a61b6df47
Taylor Burks <tayyy25@gmail.com>
2023-05-14 22:13:36 -0400

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com",
+        })
+    })
+    
+}
+
+function submitDataResponse(responseObj) {
+    fetch('http://localhost:3000/users')
+    document.createElement(responseObj)
+    .then((resp) => resp.json)
+    .then((json) => responseObj(json ))
+}
